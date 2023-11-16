5b62b2dc8c97680842a336ba640c56e355c009b1
Adithya4720 <hegdeadithyak@gmail.com>
2023-11-16 18:29:05 +0530

    Webby

---
+++ Day/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+const d = new Date();
+const day = d.getDay();
+
+var advice;
+var dayType;
+if (day == 6 || day == 0) {
+    dayType = 'a weekend';
+    advice = "Happy weekend!";
+} else {
+    dayType = 'a weekday';
+    advice = "go to work!";
+}
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        dayType: dayType,
+        advice: advice
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
